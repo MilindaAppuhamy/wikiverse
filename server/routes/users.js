3bd3ab3ceb7b08fcc83e89ed4094e193366bf5de
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-10-30 16:44:33 +0000

    Made changes for the extra features.

--- server/routes/users.js
+++ server/routes/users.js
@@ -16,15 +16,50 @@ router.get("/", async (req, res, next) => {
 router.get("/:userId", async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.userId, {
-      include: [{ model: Page }]
+      include: [{ model: Page }],
     });
 
-    if(!user) {
+    if (!user) {
       res.status(404);
       next();
     } else {
       res.send(user);
     }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (!user) {
+      res.status(404).send("Invalid user");
+      next();
+    } else {
+      if (user.password === req.body.password) {
+        res.send(user);
+      } else {
+        res.send("Incorrect password");
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.send(user);
   } catch (error) {
     next(error);
   }
--- server/seedData.js
+++ server/seedData.js
@@ -2,45 +2,47 @@ module.exports = {
   pages: [
     {
       title: "First Page",
-      content: "This is my first page. I hope I love writing articles as much as I love reading them.",
-      status: 'closed',
-      authorId: 2
+      content:
+        "This is my first page. I hope I love writing articles as much as I love reading them.",
+      status: "closed",
+      authorId: 2,
     },
     {
       title: "Duff",
       content: "This is Homer Simpson's drink of choice.",
-      status: 'closed',
-      authorId: 1
+      status: "closed",
+      authorId: 1,
     },
     {
       title: "Living the Glam Life",
       content: "Do you even? I swear that half of you are posing.",
-      status: 'open',
-      authorId: 1
+      status: "open",
+      authorId: 1,
     },
     {
       title: "Eleanor of Aquitaine",
-      content: "This medieval queen was married to both Louis VII of France and Henry II of England.",
-      status: 'open',
-      authorId: 2
+      content:
+        "This medieval queen was married to both Louis VII of France and Henry II of England.",
+      status: "open",
+      authorId: 2,
     },
   ],
   users: [
-    { name: 'Annie Position', email: 'annie@email.com' },
-    { name: 'Bea O-Problem', email: 'bea@email.com' },
+    { name: "Annie Position", email: "annie@email.com", password: "password" },
+    { name: "Bea O-Problem", email: "bea@email.com", password: "password" },
   ],
   tags: [
     {
-      name: 'happy',
+      name: "happy",
     },
     {
-      name: 'youcandoanything',
+      name: "youcandoanything",
     },
     {
-      name: 'canpeopledoeverything',
+      name: "canpeopledoeverything",
     },
     {
-      name: 'royalty',
-    }
-  ]
-}
+      name: "royalty",
+    },
+  ],
+};
