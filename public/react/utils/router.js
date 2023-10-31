beda05ae5764caa3a4cd271a6760eb20bdc4e5a2
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-10-31 15:51:37 +0000

    Landing page added.

--- public/react/utils/router.js
+++ public/react/utils/router.js
@@ -1,12 +1,15 @@
 import { Outlet, createBrowserRouter } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
+import AuthenticationPage from "../pages/AuthenticationPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Outlet />,
-    errorElement: <LandingPage />,
-    children: [{ index: true, element: <LandingPage /> }],
+    children: [
+      { index: true, element: <LandingPage /> },
+      { path: "/authenticate", element: <AuthenticationPage /> },
+    ],
   },
 ]);
 
