ffca4c407826d7402b3f265cf941f3b9cb94ea4a
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-11-06 12:28:33 +0000

    Settings page UI completed.

--- public/react/pages/AccountPage.jsx
+++ public/react/pages/AccountPage.jsx
@@ -1,6 +1,31 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import AuthContext from "../context/AuthContext";
+import axios from "axios";
+import apiURL from "../utils/api";
+import Loading from "../components/Loading";
 
 function AccountPage() {
+  const { authUser } = useContext(AuthContext);
+  const [myArticles, setMyArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const requestUrl = `${apiURL}/wiki`;
+
+  useEffect(() => {
+    const getArticles = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(requestUrl);
+        setMyArticles(res.data);
+        setLoading(false);
+      } catch (e) {
+        console.log("error", e);
+        setLoading(false);
+      }
+    };
+
+    getArticles();
+  }, []);
+
   return (
     <>
       <h1>Account page</h1>
--- public/react/pages/SettingsPage.jsx
+++ public/react/pages/SettingsPage.jsx
@@ -6,7 +6,139 @@ function SettingsPage() {
   console.log(authUser);
   return (
     <>
-      <h1>Settings page</h1>
+      <div
+        className="articles-container"
+        style={{
+          position: "absolute",
+          paddingTop: "180px",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "90%",
+            maxWidth: "900px",
+            borderRadius: "15px",
+            backgroundColor: "whitesmoke",
+            padding: "20px",
+            boxShadow: "3px 3px 8px #787878",
+          }}
+        >
+          <div
+            className="my-account-details"
+            style={{
+              margin: "10px",
+              backgroundColor: "white",
+              display: "grid",
+              gridTemplateAreas: `"name value"
+            "name value"
+            "name value"`,
+              gridTemplateColumns: "1fr 1fr",
+              gridColumnGap: "5px",
+              gridTemplateRows: "30px 30px 30px",
+              gridRowGap: "15px",
+              backgroundColor: "transparent",
+              marginBottom: "60px",
+            }}
+          >
+            <h1
+              style={{
+                gridArea: "name",
+                gridRow: "1",
+                fontFamily: "sans-serif",
+                fontSize: "20px",
+                backgroundColor: "transparent",
+                padding: "8px",
+                color: "#4a4a4a",
+              }}
+            >
+              Name
+            </h1>
+            <h1
+              style={{
+                gridArea: "value",
+                gridRow: "1",
+                fontFamily: "sans-serif",
+                fontSize: "20px",
+                backgroundColor: "transparent",
+                padding: "8px",
+                color: "#4a4a4a",
+              }}
+            >
+              {authUser.name}
+            </h1>
+            <h1
+              style={{
+                gridArea: "name",
+                gridRow: "2",
+                fontFamily: "sans-serif",
+                fontSize: "20px",
+                backgroundColor: "transparent",
+                padding: "8px",
+                color: "#4a4a4a",
+              }}
+            >
+              Email
+            </h1>
+            <h1
+              style={{
+                gridArea: "value",
+                gridRow: "2",
+                fontFamily: "sans-serif",
+                fontSize: "20px",
+                backgroundColor: "transparent",
+                padding: "8px",
+                color: "#4a4a4a",
+              }}
+            >
+              {authUser.email}
+            </h1>
+            <h1
+              style={{
+                gridArea: "name",
+                gridRow: "3",
+                fontFamily: "sans-serif",
+                fontSize: "20px",
+                backgroundColor: "transparent",
+                padding: "8px",
+                color: "#4a4a4a",
+              }}
+            >
+              Password
+            </h1>
+            <h1
+              style={{
+                gridArea: "value",
+                gridRow: "3",
+                fontFamily: "sans-serif",
+                fontSize: "20px",
+                backgroundColor: "transparent",
+                padding: "8px",
+                color: "#4a4a4a",
+              }}
+            >
+              {authUser.password}
+            </h1>
+          </div>
+
+          <div
+            className="my-account-actions"
+            style={{
+              backgroundColor: "transparent",
+              gap: "20px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <button className="logout-button">Logout</button>
+            <button className="delete-account-button">Delete account</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
